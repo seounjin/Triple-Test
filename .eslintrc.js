a51fba703d97c4254305aadf10b9cbd81f1fa16f
qustj <invanda7@gmail.com>
2022-06-15 15:34:49 +0900

    SETTING : CRA 프로젝트생성 / titicacadev/eslint-config-triple 적용

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+const createConfig = require('@titicaca/eslint-config-triple/create-config')
+
+const { extends: extendConfigs, overrides } = createConfig({
+  type: 'frontend',
+  project: './tsconfig.json',
+})
+
+module.exports = {
+  extends: [
+    ...extendConfigs,
+    // 확장할 규칙 이름...
+  ],
+  overrides: [
+    ...overrides,
+    // 특정 파일 대상 규칙...
+  ],
+  rules: {
+    // 개별 규칙
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+}
